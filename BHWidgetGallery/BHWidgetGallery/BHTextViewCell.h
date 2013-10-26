5910c62ca3aeea00728df12b69ecaf5903a077a1
Bryan Hansen <bryguy1300@gmail.com>
2013-10-25 21:53:03 -0700

    Now targeting iOS 7.

---
+++ BHWidgetGallery/BHWidgetGallery/BHTextViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BHTextViewCell.h
+//  BHWidgetGallery
+//
+//  Created by Bryan Hansen on 10/25/13.
+//  Copyright (c) 2013 skeuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BHTextViewCell : UITableViewCell
+
+@end
---
+++ BHWidgetGallery/BHWidgetGallery/BHTextViewCell.m
@@ -0,0 +1,29 @@
+//
+//  BHTextViewCell.m
+//  BHWidgetGallery
+//
+//  Created by Bryan Hansen on 10/25/13.
+//  Copyright (c) 2013 skeuo. All rights reserved.
+//
+
+#import "BHTextViewCell.h"
+
+@implementation BHTextViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
